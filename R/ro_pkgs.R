f06192bfdf2d86ff41bf9fb276d95d2efe5e0404
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-05 21:21:07 -0800

    added missing params for ro_pkgs

--- R/ro_pkgs.R
+++ R/ro_pkgs.R
@@ -4,6 +4,11 @@
 #' @importFrom jsonlite fromJSON
 #' @export
 #'
+#' @param query Query terms, uses \code{grep}
+#' @param category Category to get, matches exactly
+#' @param on_cran (logical) Whether to select only those on CRAN, default: all
+#' @param maintainer (character) A maintainer name
+#' @param simplify (logical) Simplify output to more important fields
 #' @param ... Named options passed on to \code{\link[devtools]{install}}
 #' @examples \donttest{
 #' # list all packages
@@ -13,21 +18,25 @@
 #' ro_pkgs(category = "history")
 #' ro_pkgs(category = "archeology")
 #' ro_pkgs(category = "economics")
+#' ro_pkgs(maintainer = "Scott Chamberlain")
 #' ro_pkgs(on_cran = TRUE)
 #' ro_pkgs(query = "species")
 #' ro_pkgs(query = "alfjafaksd")
 #' }
 
-ro_pkgs <- function(query = NULL, category = NULL, on_cran = NULL, simplify = FALSE, ...){
+ro_pkgs <- function(query = NULL, category = NULL, on_cran = NULL,
+  maintainer = NULL, simplify = FALSE, ...)
+{
   file <- tempfile(fileext = ".json")
   download(ro_registry(), destfile=file, quiet=TRUE)
   out <- jsonlite::fromJSON(file, ...)
-  if(length(roc(list(query, category, on_cran))) > 1) stop("Can only query on one term", call. = FALSE)
+  if(length(roc(list(query, category, on_cran, maintainer))) > 1) stop("Can only query on one term", call. = FALSE)
   if(!is.null(query)){
     out <- out$packages[grep(query, out$packages$description),]
     if(NROW(out) == 0) out <- NULL
   }
   if(!is.null(category)) out <- out$packages[out$packages$category %in% category, ]
+  if(!is.null(maintainer)) out <- out$packages[out$packages$maintainer %in% maintainer, ]
   if(!is.null(on_cran)) out <- out$packages[out$packages$on_cran, ]
   if(simplify) out[, c("name","type","status","category","on_cran","cran_archived")] else out
 }
