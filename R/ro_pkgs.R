8740f7b3ee36362b20a3cdf4a13ee353b1287b55
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-05 20:06:45 -0800

    moved out old code

--- R/ro_pkgs.R
+++ R/ro_pkgs.R
@@ -15,23 +15,3 @@ ro_pkgs <- function(...){
 }
 
 ro_registry <- function() 'https://raw.githubusercontent.com/sckott/roregistry/master/registry.json'
-#   out <- lapply(1:5, gh_repos)
-#   pkgs <- sort(unique(sapply(do.call(c, out), "[[", "name")))
-#
-# gh_repos <- function(page = 1, owner = "ropensci", ...) {
-#   req <- GET(sprintf("https://api.github.com/orgs/%s/repos", owner),
-#              query=list(page=page), ...)
-#   process_result(req)
-# }
-#
-# make_url <- function(x, y, z) {
-#   sprintf("https://api.github.com/repos/%s/%s/%s", x, y, z)
-# }
-#
-# process_result <- function(x) {
-#   stop_for_status(x)
-#   if (!x$headers$`content-type` == "application/json; charset=utf-8")
-#     stop("content type mismatch")
-#   tmp <- content(x, as = "text")
-#   jsonlite::fromJSON(tmp, FALSE)
-# }
